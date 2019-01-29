a5d5cd781846701aad6e915ff31a111f430f1b6b
Multimegamander <multimegamander@gmail.com>
2019-01-28 21:53:44 -0800

    made it work :>

--- main.cpp
+++ main.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
 
-int main(int argc, char const *argv[]) {
-
 //Celsius and Fahrenheit
 double tempc; //Celsius
 double tempf; //Fahrenheit
 
-//Equation for C to F, and VICE VERSA
-tempc = (tempf - 32)/1.8; //Calculator for F to C
-tempf = (tempc + 32)*0.55; //Calculator for C to F
-
-//Ask if using Celsius or Fahrenheit
-char input;
-std::cout << "Are you using [C]elsius, or [F]ahrenheit" << endl;
-cin >> input;
-if (input == 'C')
+int main()
 {
-  std::cout << "Enter the temperature in Celsius";
-  std::cin >> tempc;
-  std::cout << "The temp is " << tempf << " degrees Fahrenheit\n";
-}
-elseif (input == 'F')
-{
-  std::cout << "Enter the temperature in Fahrenheit";
-  std::cin >> tempf;
-  std::cout << "The temp is " << tempc << " degrees Celsius\n";
-}
-
-  return 0;
+  //Ask if using Celsius or Fahrenheit
+  char input;
+  std::cout << "Are you using [C]elcius, or [F]ahrenheit?" <<"\n";
+  std::cin >> input;
+  if (input == 'C')
+  {
+    std::cout << "Enter the temperature in Celsius";
+    std::cin >> tempc;
+    tempf = (tempc * 1.8)+32; //Calculator for C to F
+    std::cout << "The temp is " << tempf << " degrees Fahrenheit\n";
+  }
+  else if (input == 'F')
+  {
+    std::cout << "Enter the temperature in Fahrenheit";
+    std::cin >> tempf;
+    tempc = (tempf - 32)/0.5556; //Calculator for F to C
+    std::cout << "The temp is " << tempc << " degrees Celsius\n";
+  }
 }
